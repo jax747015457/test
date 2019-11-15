b926ccd63422d4e2fd04a5c0ba23e4c6796486f1
Evan <chengwenjax@163.com>
2019-11-15 16:41:10 +0800

    test project

---
+++ src/main/java/com/evan/test/client/test.java
@@ -0,0 +1,27 @@
+package com.evan.test.client;
+
+import com.evan.test.shared.FieldVerifier;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+
+public class test implements EntryPoint {
+    @Override
+    public void onModuleLoad() {
+        Button btn = new Button("test01");
+        RootPanel.get().add(btn);
+    }
+}
